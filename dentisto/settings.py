d673ea5bd061b7b1188b154e8def44c5bc47be6f
Saad K <shadekassimi@gmail.com>
2022-05-24 10:51:03 +0800

    home page

--- dentisto/settings.py
+++ dentisto/settings.py
@@ -141,7 +141,4 @@ EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home', views.home, name="home"),
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
     path('services/', views.services, name="services"),
